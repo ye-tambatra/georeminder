3ae9ccadc1db5bd2b2676e289205efd021b5ab4d
Tambatra <nofiniaina.tambatra@gmail.com>
2025-04-29 03:18:51 -0400

    [feat] Add reminders service functions ;add snakeToCamel utility function for object key transformation; format existing utility function for consistency

--- gr_client/src/lib/utils.ts
+++ gr_client/src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+   return twMerge(clsx(inputs));
 }
+
+export const snakeToCamel = (obj: any) => {
+   const newObj: any = {};
+   for (const key in obj) {
+      const camelKey = key.replace(/(_\w)/g, (m) => m[1].toUpperCase());
+      newObj[camelKey] = obj[key];
+   }
+   return newObj;
+};
---
+++ gr_client/src/services/reminders.ts
@@ -0,0 +1,40 @@
+import api from "@/lib/axios";
+import { snakeToCamel } from "@/lib/utils";
+
+export enum TriggerType {
+   EXIT = "exit",
+   ENTER = "enter",
+}
+
+export interface Reminder {
+   id?: number;
+   title: string;
+   description: string;
+   triggerType: TriggerType;
+   locationLat: number;
+   locationLng: number;
+}
+
+export const createReminder = async (reminder: Reminder): Promise<Reminder> => {
+   const response = await api.post<Reminder>("api/users/reminders/", reminder);
+   return snakeToCamel(response.data);
+};
+
+export const getReminders = async (): Promise<Reminder[]> => {
+   const response = await api.get<Reminder[]>("api/users/reminders/");
+   return response.data.map(snakeToCamel);
+};
+
+export const getReminderById = async (id: number): Promise<Reminder> => {
+   const response = await api.get<Reminder>(`api/users/reminders/${id}/`);
+   return snakeToCamel(response.data);
+};
+
+export const updateReminder = async (id: number, reminder: Reminder): Promise<Reminder> => {
+   const response = await api.put<Reminder>(`api/users/reminders/${id}/`, reminder);
+   return snakeToCamel(response.data);
+};
+
+export const deleteReminder = async (id: number): Promise<void> => {
+   await api.delete(`api/users/reminders/${id}/`);
+};
