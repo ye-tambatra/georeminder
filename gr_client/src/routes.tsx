67e3969ce113f1ca318b006cb84563031d1ffcfd
Tambatra <nofiniaina.tambatra@gmail.com>
2025-04-29 10:38:50 -0400

    [refactor] Restructure reminder-related components and services; move CreateReminder, UpdateReminder, and ReminderDetails to new paths for better organization

--- gr_client/src/routes.tsx
+++ gr_client/src/routes.tsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router";
 import HomePage from "@/pages/Home";
 import DashboardPage from "@/pages/Dashboard";
-import CreateReminder from "@/pages/CreateReminder";
-import UpdateReminder from "@/pages/UpdateReminder";
-import CreateUpdateReminderLayout from "./layouts/CreateUpdateReminder";
-import NotFound from "./pages/NotFound";
-import RealTimeTracking from "./pages/RealTimeTracking";
-import OAuthCallback from "./pages/callbacks/OAuthCallback";
-import ProtectedRouteLayout from "./layouts/ProtectedRoute";
-import AuthenticatedUserLayout from "./layouts/AuthenticatedUser";
-import ReminderDetails from "./pages/ReminderDetails";
+import CreateReminder from "@/pages/reminders/CreateReminder";
+import UpdateReminder from "@/pages/reminders/UpdateReminder";
+import CreateUpdateReminderLayout from "@/layouts/CreateUpdateReminder";
+import NotFound from "@/pages/NotFound";
+import RealTimeTracking from "@/pages/RealTimeTracking";
+import OAuthCallback from "@/pages/callbacks/OAuthCallback";
+import ProtectedRouteLayout from "@/layouts/ProtectedRoute";
+import AuthenticatedUserLayout from "@/layouts/AuthenticatedUser";
+import ReminderDetails from "@/pages/reminders/ReminderDetails";
 
 const AppRoutes = () => {
    return (
